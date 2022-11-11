9b4e83d9b87c43cd4045136f2f76f41f77e3a62f
Ashif <ashiful134@gmail.com>
2022-11-11 12:12:27 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
-function App() {
+function App()
+{
   return (
     <div className="App">
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          hello
         </p>
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
